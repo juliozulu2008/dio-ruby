f55c83e283e74a0429a85e4ab14d1369d5d7cbe0
Julio Cesar Dias Dos Santos <juliozulu2008@gmail.com>
2023-03-21 13:57:44 -0300

    Desafio Curso dio RUBY

---
+++ desafio.rb
@@ -0,0 +1,7 @@
+puts "Olá, digite seu nome: "
+nome = gets.chomp
+puts "Agora digite seu Sobrenome: "
+sobrenome = gets.chomp
+puts "Agora #{nome} digite sua idade: "
+idade = gets.to_i
+puts "Seja Muito Bem Vindo #{nome} #{sobrenome} sua idade é #{idade} anos!"
